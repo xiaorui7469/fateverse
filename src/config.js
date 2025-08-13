fb5664def77ce05591edebf8c00e7ffd6247ab4e
xiaorui7469 <1843936400@qq.com>
2025-08-13 16:22:55 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,7 +8,8 @@ const config = {
 	},
 
 	avatar: "/img/avatar.jpg", // 头像
-	welcometitle: "Hi,Welcome to "," FateVerse Server.", // 标题
+	welcometitle: "Hi,Welcome to",
+	" FateVerse Server.", // 标题
 
 	// 颜色配置
 	color: {
